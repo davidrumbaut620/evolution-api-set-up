394dc2cc83ca9387010b4134339c19760580fdc0
hans1801 <hansacha.1801@gmail.com>
2025-07-18 03:38:06 -0500

    last version

--- certify_domain.sh
+++ certify_domain.sh
@@ -3,7 +3,7 @@ sudo apt update
 sudo apt install -y nginx certbot python3-certbot-nginx
 
 # Paso 1: Crear configuración temporal HTTP sin SSL sobrescribiendo evolution_api
-cat > "/etc/nginx/sites-available/evolution_api" <<EOF
+sudo bash -c 'cat > "/etc/nginx/sites-available/evolution_api" <<EOF
 server {
     listen 80;
     server_name TU_DOMINIO.com;
@@ -12,7 +12,7 @@ server {
         proxy_pass http://localhost:8080;
         proxy_http_version 1.1;
         proxy_set_header Upgrade \$http_upgrade;
-        proxy_set_header Connection 'upgrade';
+        proxy_set_header Connection "Upgrade";
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
@@ -20,17 +20,18 @@ server {
         proxy_cache_bypass \$http_upgrade;
     }
 }
-EOF
+EOF'
 
 # Habilitar configuración
-ln -sf /etc/nginx/sites-available/evolution_api /etc/nginx/sites-enabled/evolution_api
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -sf /etc/nginx/sites-available/evolution_api /etc/nginx/sites-enabled/evolution_api
 sudo nginx -t && sudo systemctl reload nginx
 
 # Paso 2: Obtener certificado SSL con Certbot
 sudo certbot --nginx -d TU_DOMINIO.com
 
 # Paso 3: Reescribir configuración con versión segura (HTTPS)
-cat > "/etc/nginx/sites-available/evolution_api" <<EOF
+sudo bash -c 'cat > /etc/nginx/sites-available/evolution_api <<EOF
 server {
     listen 80;
     server_name TU_DOMINIO.com;
@@ -50,15 +51,19 @@ server {
         proxy_pass http://localhost:8080;
         proxy_http_version 1.1;
         proxy_set_header Upgrade \$http_upgrade;
-        proxy_set_header Connection 'upgrade';
+        proxy_set_header Connection "Upgrade";
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto \$scheme;
+        chunked_transfer_encoding off;
+        proxy_buffering off;
+        proxy_cache off;
         proxy_cache_bypass \$http_upgrade;
     }
 }
-EOF
+EOF'
+
 
 # Recargar NGINX con configuración final
 sudo nginx -t && sudo systemctl reload nginx
